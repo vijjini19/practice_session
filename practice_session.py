06c218a4f248f4ebccea6c0ec0e2dc03dcb3df00
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-23 22:07:20 +0530

    Add files via upload

---
+++ practice_session.py
@@ -0,0 +1,28 @@
+# i = 1
+# while i < 6:
+#     if i == 3:
+#         print(i)
+#         i += 1
+
+# fruits=["apple","banana","cherry"]
+# for x in fruits:
+#     print(x)
+
+# def my_function():
+#     print('Hello from a function')
+# my_function
+
+# def my_function(fname,lname):
+#     print(fname)
+# my_function("python",'java')
+
+# def my_function(x):
+#     return x+5
+# print(my_function(3))
+
+# def my_function(*kids):
+#     print("The youngest child is "+kids[2])
+# my_function("hd",'sd','bh','hy')
+
+# def my_function(**kid):
+#     print("His last name is "+kid["lname"])
