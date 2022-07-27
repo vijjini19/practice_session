e83437d23fa9ba999d9206bb57a8b6cee31a5ffe
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-27 21:20:34 +0530

    Add files via upload

---
+++ practice_session4.py
@@ -0,0 +1,12 @@
+# def greet():
+#     print("Hello")
+# greet()  #Hello
+
+# def greet(n):
+#     print("Hello "+n)
+# greet("sindhu")  #Hello sindhu
+# greet("vujjini") #Hello vujjini
+
+# def greet(*names):
+#     print("Hey",names[0])
+# greet("python","programming","core","Functions")  #Hey python
